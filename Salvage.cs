e5df1d82aabde2e47de01e3d28b954a091aa1977
Randactyl <rafowler@randactyl.com>
2016-05-13 14:54:18 -0700

    fix refund so it doesn't give super stacks

--- Salvage.cs
+++ Salvage.cs
@@ -64,29 +64,59 @@ namespace Oxide.Plugins {
         }
 
         void SalvageItem(BasePlayer player, Item item) {
+            //check player can craft item
         	bool canCraft = player.blueprints.CanCraft(item.info.itemid, 0);
 
-        	item.RemoveFromContainer();
-
+            //if can't craft or broken, else salvage
         	if(!canCraft || (item.hasCondition && item.condition == 0)) {
-        		player.GiveItem(item);
-        		//Puts("Giving " + player.ToString() + " " + item.ToString());
         		player.ChatMessage("This item is not salvageable.");
         	} else {
+                //remove the item from the container
+        	    item.RemoveFromContainer();
+                
+                //get bp
         		ItemBlueprint itemBp = blueprints[item.info.shortname];
+                
+                //set ratios
         		double refundRatio = 0.5;
+                //if item.hasCondition then conditionRatio == item.conditionNormalized else conditionRatio = 1
                 double conditionRatio = item.hasCondition ? item.conditionNormalized : 1;
-
+                
+                /*
+                    itemBp.amountToCreate is the number of items made by the act of crafting (3x 5.56)
+                    ingredient.amount is the number of that material required (100 wood for campfire)
+                    ingredient.itemDef.stackable is the max stack size of that ingredient
+                */
                 foreach(ItemAmount ingredient in itemBp.ingredients) {
+                    //calculate refund amount
                     double refundAmount = ingredient.amount / itemBp.amountToCreate;
                     refundAmount = refundAmount * refundRatio * conditionRatio * item.amount;
                     refundAmount = Math.Ceiling(refundAmount);
                     if(refundAmount < 1)
                         refundAmount = 1;
-
-                    Item newItem = ItemManager.Create(ingredient.itemDef, (int)refundAmount);
-                    player.GiveItem(newItem);
-                    //Puts("Giving " + player.ToString() + " " + newItem.ToString());
+                        
+                    //calculate how many stacks to split the refund into (no super stacks)
+                    double refundStacks = refundAmount / ingredient.itemDef.stackable;
+                    if(refundStacks < 1)
+                        refundStacks = 1;
+                    
+                    //refund ingredient
+                    for(int i = 0; i < refundStacks; i++) {
+                        int amt;
+                        
+                        //calculate how much to give in this stack
+                        if (refundAmount <= ingredient.itemDef.stackable)
+                            amt = (int)refundAmount;
+                        else
+                            amt = ingredient.itemDef.stackable;
+                        
+                        //reduce the pending refund amount by how much we're about to give out
+                        refundAmount = refundAmount - amt;
+                        
+                        //create and give this stack
+                        Item newItem = ItemManager.Create(ingredient.itemDef, amt);
+                        player.GiveItem(newItem);
+                    }
                 }
         	}
         }
